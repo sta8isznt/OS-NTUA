d6f938304beb8f5f8dd438d9cb2a41477bc801be
Stathis Zontos <stathis.zontos@gmail.com>
2026-03-24 22:36:57 +0200

    Added codes for Exercise 1

---
+++ Ex1s/Final/utils.c
@@ -0,0 +1,49 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+void write_message(int fd, const char *message){
+    size_t len = strlen(message);
+    ssize_t wcnt;
+    size_t idx = 0;
+
+    do {
+        wcnt = write(fd, message + idx, len - idx);
+        if (wcnt == -1) {
+            return;
+        }
+        idx += wcnt;
+    } while (idx < len);
+}
+
+void write_int(int fd, const int *x){
+    ssize_t wcnt;
+    size_t idx = 0;
+
+    do {
+        wcnt = write(fd, message + idx, len - idx);
+        if (wcnt == -1) {
+            return;
+        }
+        idx += wcnt;
+    } while (idx < len);
+}
+
+ssize_t read_all(int fd, void *buf, size_t count){
+    size_t total = 0;
+    ssize_t n;
+
+    while (total < count) {
+        n = read(fd, (char *)buf + total, count - total);
+        if (n == -1) {
+            return -1;
+        }
+        if (n == 0) {
+            break;
+        }
+        total += n;
+    }
+
+    return total;
+}
+
