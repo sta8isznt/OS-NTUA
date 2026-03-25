60f078f5571f64ed188275f7f3fb664a63daf7e3
Stathis Zontos <stathis.zontos@gmail.com>
2026-03-25 15:13:50 +0200

    Added protocol header

---
+++ Exercise1/Ex4/protocol.h
@@ -0,0 +1,29 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+#include <sys/types.h>
+
+/* List of all the possible signal types */
+typedef enum {
+    CMD_ADD_WORKER,
+    CMD_REMOVE_WORKER,
+    CMD_LIST,
+    CMD_PROGRESS,
+    CMD_SHUTDOWN,
+    CMD_ASSIGN_WORK,
+    CMD_WORK_RESULT
+} msg_type_t;
+
+/* struct for messages between modules */
+typedef struct {
+    msg_type_t type;
+    int value;          /* general field (e.g. how many workers to add) */
+    pid_t pid;
+    off_t offset;       /* file offset */
+    size_t length;      /* count of bytes */
+    char target;        /* searching character */
+    int count;          /* worker result */
+    int job_id;
+} message_t;
+
+#endif
